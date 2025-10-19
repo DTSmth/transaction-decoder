89571f0fab662daf8c153f7e8acacbc8fc03278f
DTSmth <ducktapedave85@gmail.com>
2025-10-19 16:59:39 -0400

    create loop to read the entire input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::io::Read;
 
-
 fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
     let mut compact_size = [0_u8; 1];
     transaction_bytes.read(&mut compact_size).unwrap();
@@ -24,29 +23,49 @@ fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
         }
     }
 }
-fn read_version(transaction_bytes: &mut &[u8]) -> u32 {
+fn read_u32(transaction_bytes: &mut &[u8]) -> u32 {
 
     let mut buffer = [0; 4];
     transaction_bytes.read(&mut buffer).unwrap();
     u32::from_le_bytes(buffer)
 }
+fn read_txid(transaction_bytes: &mut &[u8]) -> [u8; 32] {
+    let mut buffer = [0; 32];
+    transaction_bytes.read(&mut buffer).unwrap();
+    buffer.reverse();
+    buffer
+}
+fn read_script(transaction_bytes: &mut &[u8]) -> Vec<u8> {
+    let script_size = read_compact_size(transaction_bytes) as usize;
+    let mut buffer = vec![0; script_size];
+    transaction_bytes.read(&mut buffer).unwrap();
+    buffer
+}
+
 fn main() {
     let transaction_hex = "02000000000101d09027b7704128d6168122212afbcb5d960e572f59a879d60dc53370f382cab40000000000fdffffff0227be16000000000016001498779af6da1c0b3bf20be047a1b6e3491ab991790000000000000000156a5d1214011400ff7f818cec82d08bc0a88281d215024730440220233215e028be213467a97eac37951a8e9bd2b41e6630107ad3b8f5ceb7dbcc4a0220320e24c70fafcab86dbc4fcdd6f3fddcd6e6f54e785631ddfbe00d1a89fff4d20121024f5b5d052d4d76b514bacbd8407e6297f4043739e9f359ee3c9baa716332aafc00000000";
 
     let transaction_bytes = hex::decode(transaction_hex).unwrap();
     let mut bytes_slice = transaction_bytes.as_slice();
 
-    let version = read_version(&mut bytes_slice);
+    let version = read_u32(&mut bytes_slice);
 
     // Skip marker and flag (2 bytes)
     let mut segwit_header = [0u8; 2];
     bytes_slice.read(&mut segwit_header).unwrap();
 
 
-    let input_length = read_compact_size(&mut bytes_slice);
+    let input_count = read_compact_size(&mut bytes_slice);
+
+    for _ in 0..input_count {
+        let txid = read_txid(&mut bytes_slice);
+        let output_index = read_u32(&mut bytes_slice);
+        let script_sig = read_script(&mut bytes_slice);
+        let sequence = read_u32(&mut bytes_slice);
+    }
 
     println!("version: {}", version);
-    println!("input_length: {}", input_length);
+    println!("input_count: {}", input_count);
 }
 
 #[cfg(test)]
